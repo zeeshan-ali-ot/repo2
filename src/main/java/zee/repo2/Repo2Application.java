16a57262b809556c2f18b9c853cf3d82ca7f3a79
Zeeshan Ali <zeeshan.ali_ot@crewlogix.com>
2025-11-19 12:21:39 +0500

    first commit

---
+++ src/main/java/zee/repo2/Repo2Application.java
@@ -0,0 +1,13 @@
+package zee.repo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Repo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/zee/repo2/Repo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package zee.repo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Repo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
